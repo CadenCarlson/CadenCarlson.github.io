9ea1bb4269e40add0126192e30454f5127954595
Caden Carlson <cadencarlson1@gmail.com>
2023-01-01 11:31:05 -0600

    AOS

--- script/script.js
+++ script/script.js
@@ -22,7 +22,7 @@ function reveal() {
     if (elementTop < windowHeight - elementVisible) {
       reveals[i].classList.add("active");
     } else {
-      reveals[i].classList.remove("active");
+      //reveals[i].classList.remove("active");
     }
   }
 }
