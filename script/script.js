2b3ba669e369de348053cb59afd727707ec63500
Caden Carlson <cadencarlson1@gmail.com>
2023-01-01 12:09:20 -0600

    delete old animation code

--- script/script.js
+++ script/script.js
@@ -10,23 +10,6 @@ var NavbarPortfolio = document.getElementById("navbarPortfolio");
 var NavbarContact = document.getElementById("navbarContact");
 let lastHighlight = NavbarHome;
 
-// Handles Scroll Animation
-function reveal() {
-  var reveals = document.querySelectorAll(".reveal");
-
-  for (var i = 0; i < reveals.length; i++) {
-    var windowHeight = window.innerHeight;
-    var elementTop = reveals[i].getBoundingClientRect().top;
-    var elementVisible = 150;
-
-    if (elementTop < windowHeight - elementVisible) {
-      reveals[i].classList.add("active");
-    } else {
-      //reveals[i].classList.remove("active");
-    }
-  }
-}
-
 // Handles Welcome Animation
 function welcome() {
   setTimeout(()=>{
@@ -83,7 +66,6 @@ function navbarHighlight() {
 
 
 // Event Listeners
-window.addEventListener("scroll", reveal);
 window.addEventListener('DOMContentLoaded', welcome);
 window.addEventListener('scroll', navbarHighlight);
 
